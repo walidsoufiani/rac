9bc24efc0b0f5705718f3a2422b588dcdd5d3b01
walidsoufiani <140754926+walidsoufiani@users.noreply.github.com>
2023-08-05 17:54:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,25 @@ import io
 import requests
 
 # load the environment variables
-load_dotenv()
+def on_api_key_change():
+	api_key = ss.get('api_key') or os.getenv('OPENAI_KEY')
+	model.use_key(api_key) # TODO: empty api_key
+	#
+	if 'data_dict' not in ss: ss['data_dict'] = {} # used only with DictStorage
+	ss['storage'] = storage.get_storage(api_key, data_dict=ss['data_dict'])
+	ss['cache'] = cache.get_cache()
+	ss['user'] = ss['storage'].folder # TODO: refactor user 'calculation' from get_storage
+	model.set_user(ss['user'])
+	ss['feedback'] = feedback.get_feedback_adapter(ss['user'])
+	ss['feedback_score'] = ss['feedback'].get_score()
+	#
+	ss['debug']['storage.folder'] = ss['storage'].folder
+	ss['debug']['storage.class'] = ss['storage'].__class__.__name__
 
+
+ss['community_user'] = os.getenv('COMMUNITY_USER')
+if 'user' not in ss and ss['community_user']:
+	on_api_key_change() # use community key
 
 # Create the vectorstore directory if it doesn't exist
 VECTORSTORE_DIR = "vectorstore"
